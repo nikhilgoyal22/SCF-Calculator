0affc9f13dca18be966419f9cc57002d02991852
Nikhil Goyal <nikhil.goyal@navyuginfo.com>
2013-04-30 19:09:27 +0530

    making widget

---
+++ date-support.js
@@ -0,0 +1,21 @@
+Date.prototype.addDays = function(days) {
+    this.setDate(this.getDate() + days);
+    return this;
+};
+
+Date.prototype.getMonthName = function(lang) {
+	lang = lang && (lang in Date.locale) ? lang : 'en';
+	return Date.locale[lang].month_names[this.getMonth()];
+};
+
+Date.prototype.getMonthNameShort = function(lang) {
+    lang = lang && (lang in Date.locale) ? lang : 'en';
+    return Date.locale[lang].month_names_short[this.getMonth()];
+};
+
+Date.locale = {
+    en: {
+       month_names: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+       month_names_short: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    }
+};
