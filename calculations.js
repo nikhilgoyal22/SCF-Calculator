eaa239721ac6640ed8502bd1789022aa527f31a5
Nikhil Goyal <nikhil.goyal@navyuginfo.com>
2013-05-02 20:00:27 +0530

    calculator widget calculations implemented

---
+++ calculations.js
@@ -0,0 +1,28 @@
+yearDays = 360;
+
+calculations = {
+	"preScf" : {
+		"title" : "Pre SCF Financing Costs:",
+		"formula" : "(Invoice Value/" + yearDays + ") * Net Days * Supplier Value of Capital %"
+	},
+	"postScf" : {
+		"title" : "Post SCF Financing based on SCF Financing % APR:",
+		"formula" : "(Invoice Value/" + yearDays + ") * Days Paid Early * SCF Financing %"
+	},
+	"postScfScc" : {
+		"title" : "Post SCF Financing based on SCF Financing % APR:",
+		"formula" : "(Invoice Value/" + yearDays + ") * Days Paid Early * SCF Financing % + (Invoice Value/" + yearDays + ") * Day After Invoice to Pay * Supplier Value of Capital %"
+	},
+	"costReduction" : {
+		"title" : "Supply Chain Price Reduction Potential:",
+		"formula" : "Pre SCF Financing Costs - Post SCF Financing based on SCF Financing % APR"
+	},
+	"improvement" : {
+		"title" : "Percent Improvement:",
+		"formula" : "(Supply Chain Price Reduction Potential/Pre SCF Financing Costs)*100 %"
+	},
+	"accountCash" : {
+		"title" : "Cash in your account on XXXX date",
+		"formula" : "Invoice Value - Post SCF Financing based on SCF Financing % APR"
+	}
+}
